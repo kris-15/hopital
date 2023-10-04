cb099af4ce20424cda28a8ad6471e5c68173718f
k-ris <christkuzanuka@gmail.com>
2023-10-04 17:22:26 +0100

    Adaptation auth requiremnts

--- controleur/auth.requirement.php
+++ controleur/auth.requirement.php
@@ -4,5 +4,6 @@ if(isset($_SESSION['medecin'])){
     header('location: index.admin.php');
 }
 require '../modele/Authentification.php';
+require '../modele/Receptionniste.php';
 $auth = new Authentification();
 ?>
